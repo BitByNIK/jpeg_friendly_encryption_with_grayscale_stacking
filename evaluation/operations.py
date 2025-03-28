842a4cfa1c28c793e90ae41c721567d09b976b55
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-03-28 20:46:21 +0530

    added rate distortion curve eval

--- evaluation/operations.py
+++ evaluation/operations.py
@@ -1,3 +1,5 @@
+from PIL import Image
+from io import BytesIO
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.measure import shannon_entropy
@@ -26,5 +28,33 @@ def plot_histograms(encrypted_a: np.ndarray, encrypted_b: np.ndarray) -> None:
         f"Histogram: Encryption B \nEntropy: {shannon_entropy(encrypted_b):.4f} bits")
     plt.xlabel('Pixel value')
 
+    plt.tight_layout()
+    plt.show()
+
+
+def get_rate_distortion_curve(encrypted_a: np.ndarray, encrypted_b: np.ndarray, qualities: list = [100, 90, 80, 70, 60, 50]) -> None:
+    def get_sizes(img_array):
+        sizes = []
+        for q in qualities:
+            img = Image.fromarray(img_array)
+            buffer = BytesIO()
+            img.save(buffer, format='JPEG', quality=q)
+            size_kb = len(buffer.getvalue()) / 1024
+            sizes.append(size_kb)
+        return sizes
+
+    sizes_no_xor = get_sizes(encrypted_a)
+    sizes_xor = get_sizes(encrypted_b)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(qualities, sizes_no_xor, marker='o',
+             label='Encrypted Without XOR')
+    plt.plot(qualities, sizes_xor, marker='x', label='Encrypted With XOR')
+    plt.xlabel('JPEG Quality Factor')
+    plt.ylabel('File Size (KB)')
+    plt.title('Rate-Distortion Curve')
+    plt.gca().invert_xaxis()
+    plt.grid(True)
+    plt.legend()
     plt.tight_layout()
     plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from crypto.operations import encrypt, encrypt_with_xor
 from graphic.operations import preprocess
 from graphic.utils import generate_jpeg_image
-from evaluation.operations import compare_encryptions, plot_histograms
+from evaluation.operations import compare_encryptions, plot_histograms, get_rate_distortion_curve
 from config import INPUT_IMAGE_PATH
 
 blocks, img_size = preprocess(INPUT_IMAGE_PATH)
@@ -12,4 +12,4 @@ generate_jpeg_image(encrypted_image_np, "test_e.jpg")
 encrypted_image_with_xor_np = encrypt_with_xor(blocks, img_size)
 generate_jpeg_image(encrypted_image_with_xor_np, "test_e_xor.jpg")
 
-plot_histograms(encrypted_image_np, encrypted_image_with_xor_np)
+get_rate_distortion_curve(encrypted_image_np, encrypted_image_with_xor_np)
